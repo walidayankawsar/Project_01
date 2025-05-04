bb156876457321e209f78b6fec754aa70402d009
walidayankawsar <walidayankawsar@gmail.com>
2025-05-03 23:16:12 -0700

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&t5$cketqnql#@zmgm-()w4lp^t-sd^l9_9=h-moqp7--#o-_f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
