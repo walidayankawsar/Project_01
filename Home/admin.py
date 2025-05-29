b37b27b9da1befae65552568a0c618110b429de5
walidayankawsar <walidayankawsar@gmail.com>
2025-05-29 12:38:00 -0700

    Resolve conflicts

--- Home/admin.py
+++ Home/admin.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 
 # Register your models here.
@@ -10,17 +9,4 @@ from . models import Clients, Work, Services, Profile
 admin.site.register(Clients)
 admin.site.register(Work)
 admin.site.register(Services)
-=======
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from . models import Clients, Work, Services, Profile
-# Register your models here.
-
-
-admin.site.register(Clients)
-admin.site.register(Work)
-admin.site.register(Services)
->>>>>>> f64541b55aabf381935de6ce0c55bdd0bfee1421
 admin.site.register(Profile)
--- Home/apps.py
+++ Home/apps.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Home'
-=======
-from django.apps import AppConfig
-
-
-class HomeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Home'
->>>>>>> f64541b55aabf381935de6ce0c55bdd0bfee1421
