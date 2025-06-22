213fa51d05ac74fd195bc6b656e765e1a4689ed1
walidayankawsar <walidayankawsar@gmail.com>
2025-06-22 08:17:32 -0700

    temp

--- Home/urls.py
+++ Home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import create_superuser
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('create_admin/', create_superuser),
 ]
--- Home/views.py
+++ Home/views.py
@@ -8,3 +8,13 @@ def home(request):
     profile = Profile.objects.first()
     return render(request, 'index.html', {'clients':client,'works':work,'service':services, 'profile':profile})
 
+
+from django .contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_superuser(request):
+    if not User.objects.filter(username="kawsar").exists():
+        User.objects.create_superuser('kawsar', 'kawsar@gmail.com', '1234.com')
+        return HttpResponse("Superuser created.")
+    else:
+        return HttpResponse("Superuser already exists.")
