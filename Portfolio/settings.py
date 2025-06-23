eb750d46d17f0d46d84fa7157968acaf5d5bb7d8
walidayankawsar <walidayankawsar@gmail.com>
2025-06-23 01:07:44 -0700

    Fix static files after Cloudinary setup

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -156,16 +156,10 @@ USE_TZ = True
 
 
 
-import os
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # যদি আপনার source static ফাইল থাকে
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # collectstatic এখানে কপি করবে
-
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
