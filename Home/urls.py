d03506d83af5aee5f1a0a7013a3c9b93762f6816
walidayankawsar <walidayankawsar@gmail.com>
2025-06-23 05:00:36 -0700

    updated

--- Home/urls.py
+++ Home/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('/admin', admin.site.urls),
 ]
