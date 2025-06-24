51d32fad3c0037ec280fd090f6f7af1775b6f6c1
walidayankawsar <walidayankawsar@gmail.com>
2025-06-24 07:49:08 -0700

    message option added

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -189,3 +189,13 @@ CSRF_TRUSTED_ORIGINS = [
     'https://walid-ayaan-kawsar.onrender.com',
 ]
 
+
+
+# Send Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
