23857d3dc962c3b1465ef2b6575d9956adc14bc1
walidayankawsar <walidayankawsar@gmail.com>
2025-11-11 04:51:18 -0800

    models created

--- home/models.py
+++ home/models.py
@@ -32,6 +32,5 @@ class Photo(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='photos/')
 
-    def __str__(self):
-        return self.title
+
 
