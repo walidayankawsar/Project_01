41dcdcc1c0fae7d8cd51f94510fe9c70cce87366
walidayankawsar <walidayankawsar@gmail.com>
2025-11-12 03:03:44 -0800

    updated

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from .views import send_message
+from django.http import HttpResponse
+
 
 urlpatterns = [
-    path('', views.mainpage, name ='mainpage'),
+    path('', views.mainpage, name="mainpage"),
+    path('send-message/', send_message, name='send_message'),
+    path('message-sent/', lambda r: HttpResponse("Message sent successfully!"), name='message_sent'),
 ]
+
+
